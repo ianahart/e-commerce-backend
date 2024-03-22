e2ca6ba51174d75b289b63f903bbd7f27cc42383
Ian Hart <ianhart7790@gmail.com>
2024-03-22 11:33:39 -0400

    feat: create models for Category, Product, ProductTag, and Tag. Create associations.

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,26 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config
 const sequelize = require('../config/connection.js');
 
+// Initialize Category model (table) by extending off Sequelize's Model class
 class Category extends Model {}
 
+// set up fields and rules for Category model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,23 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// Initialize Tag model (table) by extending off Sequelize's Model class
 const sequelize = require('../config/connection.js');
 
+// set up fields and rules for Tag model
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
